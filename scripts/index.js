715d9c10d73527ed72d6a103a57aeb0ecb022390
Boris Suganov <suganoff@yandex.ru>
2024-02-07 16:51:25 +0300

    fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,22 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
-/**
- * Ф-ция создает карточку
- * @param {Event} event
- */
-const createCard = card => {
-  let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+const cardElements = initialCards.map( card => {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   cardElement.querySelector('.card__image').src = card.link;
   cardElement.querySelector('.card__image').alt = `На фотке ${card.name}`;
   cardElement.querySelector('.card__title').textContent = card.name;
   cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-  placesList.append(cardElement);
-}
+  return cardElement;
+});
 
-/**
- * Ф-ция удаляет карточку
- * @param {Event} event
- */
 function deleteCard(event) {
   const cardItem = event.target.closest('.card');
   cardItem.remove();
 }
 
-initialCards.forEach(createCard);
+
+cardElements.forEach((card) => {
+  placesList.append(card);
+})
