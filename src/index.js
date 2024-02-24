4906cd4c17197f30aedbfe606493ba28fb1f378d
Boris Suganov <suganoff@yandex.ru>
2024-02-24 18:02:38 +0300

    set up to new projects

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// index.js
+
+import './avatar.jpg';
+import './style.css';
+
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10
